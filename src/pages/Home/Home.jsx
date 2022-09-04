fa0f9faec27bfbbad1e4b3d802b01ccbed75334d
Shoaib Afzaal <100206484+ShibCode@users.noreply.github.com>
2022-09-04 17:17:49 +0500

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,84 +15,85 @@ const Home = () => {
 
   return (
     <div className="home">
-      <div className="home__header">
-        <div>
-          <FontAwesomeIcon icon={faUser} />
+      <div>
+        <div className="home__header">
+          <div>
+            <FontAwesomeIcon icon={faUser} />
+          </div>
         </div>
-      </div>
 
-      <div className="home__topWrapper">
-        <div className="home__top">
-          <h1 className="home__topHeading">Hi, Welcome to Labz</h1>
+        <div className="home__topWrapper">
+          <div className="home__top">
+            <h1 className="home__topHeading">Hi, Welcome to Labz</h1>
 
-          <h1 className="home__topSubheading">Let's get started</h1>
+            <h1 className="home__topSubheading">Let's get started</h1>
 
-          <p className="home__topPara">
-            Since you have no wallets in <span>Labz</span> yet, choose your
-            favourite blockchain. Then, create a new wallet or import an
-            existing one.
-          </p>
+            <p className="home__topPara">
+              Since you have no wallets in <span>Labz</span> yet, choose your
+              favourite blockchain. Then, create a new wallet or import an
+              existing one.
+            </p>
+          </div>
         </div>
+
+        <div
+          className={`home__bottom ${selectedBlockChain ? "" : "show-bottom"}`}
+        >
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            Bitcoin
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            Ethereum
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            VeChain
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            Litecoin
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            GoChain
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            Æternity
+          </button>
+
+          <button
+            className="home__blockchain"
+            onClick={(e) => setSelectedBlockChain(e.target.textContent)}
+          >
+            Neo
+          </button>
+        </div>
+
+        <Blockchain
+          setSelectedBlockChain={setSelectedBlockChain}
+          blockchain={selectedBlockChain}
+        />
       </div>
-
-      <div
-        className={`home__bottom ${selectedBlockChain ? "" : "show-bottom"}`}
-      >
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          Bitcoin
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          Ethereum
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          VeChain
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          Litecoin
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          GoChain
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          Æternity
-        </button>
-
-        <button
-          className="home__blockchain"
-          onClick={(e) => setSelectedBlockChain(e.target.textContent)}
-        >
-          Neo
-        </button>
-      </div>
-
-      <Blockchain
-        setSelectedBlockChain={setSelectedBlockChain}
-        blockchain={selectedBlockChain}
-      />
-
       <Footer />
     </div>
   );
